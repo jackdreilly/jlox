6b4350653fc94f634496de7c56c6cec7e44f8bc9
Jack Reilly <jackdreilly@gmail.com>
2022-10-12 10:50:41 +0200

    almost evaluate functions

--- test/interpreter_test.dart
+++ test/interpreter_test.dart
@@ -9,6 +9,7 @@ import 'package:test/test.dart';
 import 'helpers.dart';
 
 void main() {
+  test('times_2', () => 'times_2'.prog.state['times_2']([5])?.equals(10));
   test('fib', () => expect('fib'.prog.state['fib'], isNotNull));
   test('break two levels', () => 'break_two'.prog.interpreted.equals(6));
   test('break', () => 'break'.prog.interpreted.equals(3));
